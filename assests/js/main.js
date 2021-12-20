048e95a07cfea408672cfc262227beb9fb5cf44d
Md Mourshad <77190784+MDMOURSHAD@users.noreply.github.com>
2021-12-20 17:38:41 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,57 +1,77 @@
 // Preloader 
-function load(){
+function load() {
 
 	var loading = document.getElementsByClassName('preloader');
 
 	loading[0].style.display = "none";
 }
 
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
 	// Sticky Menu
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 
-		if($(this).scrollTop() > 50){
+		if ($(this).scrollTop() > 50) {
 			$('.nav').addClass('sticky');
-		}
-
-		else{
+		} else {
 			$('.nav').removeClass('sticky');
 		}
 
 	});
 
 	// Scroll to Top
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 
-		if($(this).scrollTop() > 100){
+		if ($(this).scrollTop() > 100) {
 			$('.scroll-top').fadeIn();
-		}
-
-		else{
+		} else {
 			$('.scroll-top').fadeOut();
 		}
 
 	});
 
 
-
 	// Type js
 	var typed = new Typed('.element', {
-	  strings: ["Web Designer.", "Freelancer."],
-	  typeSpeed: 80,
-	  loop:true,
-	  backSpeed: 80,
-	  showCursor: false
+		strings: ["Web Designer.", "Freelancer."],
+		typeSpeed: 80,
+		loop: true,
+		backSpeed: 80,
+		showCursor: false
 	});
 
 	// Skill Bar
-	$('#bar1').barfiller({barColor: '#e21e51',tooltip: false,duration: 3000,animateOnResize:true});
-	$('#bar2').barfiller({barColor: '#e21e51',tooltip: false,duration: 3000});
-	$('#bar3').barfiller({ barColor: '#e21e51',tooltip: false,duration: 3000});
-	$('#bar4').barfiller({ barColor: '#e21e51', duration: 3000,tooltip: false});
-	$('#bar5').barfiller({barColor: '#e21e51',tooltip: false,duration: 3000});
-	$('#bar6').barfiller({barColor: '#e21e51',tooltip: false,duration: 3000});
-				
+	$('#bar1').barfiller({
+		barColor: '#e21e51',
+		tooltip: false,
+		duration: 3000,
+		animateOnResize: true
+	});
+	$('#bar2').barfiller({
+		barColor: '#e21e51',
+		tooltip: false,
+		duration: 3000
+	});
+	$('#bar3').barfiller({
+		barColor: '#e21e51',
+		tooltip: false,
+		duration: 3000
+	});
+	$('#bar4').barfiller({
+		barColor: '#e21e51',
+		duration: 3000,
+		tooltip: false
+	});
+	$('#bar5').barfiller({
+		barColor: '#e21e51',
+		tooltip: false,
+		duration: 3000
+	});
+	$('#bar6').barfiller({
+		barColor: '#e21e51',
+		tooltip: false,
+		duration: 3000
+	});
+
 
 });
